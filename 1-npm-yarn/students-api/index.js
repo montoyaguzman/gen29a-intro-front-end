be156befd0eeba8cb2a0c86cecc28206713397a3
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-01-23 21:34:12 -0600

    feat: avance de npm

---
+++ 1-npm-yarn/students-api/index.js
@@ -0,0 +1,4 @@
+// codigo para levantar nuestra API en expressJS
+
+// 1. Crear un endpoint GET en express que devuelta una lista estudiantes.
+// 2. La lista de estudiantes puede ser un arreglo guardado en una variable
