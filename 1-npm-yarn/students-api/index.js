e7590cd77f7af65bc10519cfe32816b40ccb1c4b
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-01-25 19:41:39 -0600

    feat: npm y semver

--- 1-npm-yarn/students-api/index.js
+++ 1-npm-yarn/students-api/index.js
@@ -2,3 +2,24 @@
 
 // 1. Crear un endpoint GET en express que devuelta una lista estudiantes.
 // 2. La lista de estudiantes puede ser un arreglo guardado en una variable
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const songs = [
+    { id: 1, name: 'Kilometros', artist: 'Los Caligaris' },
+    { id: 2, name: 'Closer', artist: 'The Chainsmokers' },
+    { id: 3, name: 'Solo a Terceros', artist: 'Panda' },
+    { id: 4, name: 'Back in black', artist: 'AC/DC' },
+    { id: 5, name: 'Ella me levanto', artist: 'DY' },
+];
+
+// localhost:8080/
+app.get('/', (request, response) => {
+    response.json(songs);
+});
+
+app.listen(port, () => {
+    console.log('servidor iniciado...')
+});
