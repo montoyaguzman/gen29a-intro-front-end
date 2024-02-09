304cc716824e02a6c2172c665cd77f93b1d1789a
CesarDev96 <cesar@crecy.io>
2024-02-08 19:36:06 -0600

    Se termina de crear el header

--- 6-router-rick-and-morty-api/src/components/Header/index.jsx
+++ 6-router-rick-and-morty-api/src/components/Header/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <nav className="header">
+        <a className="header-logo" href="#">Rick And Morty Api</a>
+        <ul className="header-nav-list">
+          <li>
+            <a className="header-list-link" href="">Home</a>
+          </li>
+          <li>
+            <a className="header-list-link" href="">Characters</a>
+          </li>
+          <li>
+            <a className="header-list-link" href="">Episodes</a>
+          </li>
+          <li>
+            <a className="header-list-link" href="">Locations</a>
+          </li>
+          <li>
+            <a className="header-list-link is-active" href="">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
